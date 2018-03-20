f12a8328e85a9152c55f6884dd81b609d5500ebf
Viacheslav Panchuk <pevmel@gmail.com>
2018-03-20 14:45:15 +0200

    This is the new re-created one commit

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ end
 gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
+gem 'devise'
+gem 'milia'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -24,9 +26,11 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
+gem 'therubyracer'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
@@ -54,3 +58,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'activerecord-session_store', github: 'rails/activerecord-session_store'
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,25 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SaasPorjectApp
+module SaasProjectApp
   class Application < Rails::Application
+
+    # uncomment to ensure a common layout for devise forms
+    #   config.to_prepare do   # Devise
+    #     Devise::SessionsController.layout "sign"
+    #     Devise::RegistrationsController.layout "sign"
+    #     Devise::ConfirmationsController.layout "sign"
+    #     Devise::PasswordsController.layout "sign"
+    #   end   # Devise
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
